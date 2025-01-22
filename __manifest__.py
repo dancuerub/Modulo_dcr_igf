f2fdb81e2c71584f971110601dd90aff0b1a410f
root <root@ip-172-31-28-50.ec2.internal>
2025-01-22 22:09:07 +0000

    Estructura modulo

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Suplementos Nutricionales',  # Nombre amigable de tu módulo
+    'version': '1.0',
+    'category': 'Real Estate',
+    'summary': 'Gestor de Suplementos Nutricionales',  # Breve descripción
+    'description': """
+    Módulo de Odoo para gestionar una tienda de suplementos.
+    Permite gestionar inventario, compra y venta de productos nutricionales.
+    """,
+    'author': 'Daniel Cuenca Rubio e Iker González Fernández',  # Tu nombre
+    'website': 'https://www.nutripertec.com',
+    'depends': ['base'],  # Dependencia base mínima
+    'data': [
+        'security/ir.model.access.csv',  # Permisos de acceso
+        # Aquí irán los archivos XML de vistas y datos más adelante
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
